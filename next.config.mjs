fd2ea534b55194ee8484decd75221786693275fc
Rodrigo Pombo <pombopombopombo@gmail.com>
2023-06-15 15:48:48 +0000

    Add theme

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 import { remarkCodeHike } from "@code-hike/mdx";
-import rehypeSlug from "rehype-slug";
+
 import mdxConfig from "@next/mdx";
 
 export default mdxConfig({
   extension: /\.mdx?$/,
-  options: { rehypePlugins: [rehypeSlug], remarkPlugins: [remarkCodeHike] },
+  options: { remarkPlugins: [[remarkCodeHike, { theme: "github-from-css" }]] },
 })({
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 });
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import "@code-hike/mdx/dist/index.css";
 import "../global.css";
+import Head from "next/head";
 
 function App({ Component, pageProps }) {
   return (
     <main>
+      <Head>
+        <title>OpenAI Chat Completion Unofficial Docs</title>
+        <script dangerouslySetInnerHTML={{ __html: themeScript }} />
+      </Head>
+      <ThemeChanger />
       <header />
       <Component {...pageProps} />
       <footer />
@@ -11,4 +17,27 @@ function App({ Component, pageProps }) {
   );
 }
 
+function setTheme(theme) {
+  document.documentElement.setAttribute("data-theme", theme);
+  localStorage.setItem("theme", theme);
+}
+const themeScript = `
+(function() {
+  try {
+    var theme = localStorage.getItem("theme");
+    if (theme) {
+      document.documentElement.setAttribute("data-theme", theme);
+    }
+  } catch (e) {}
+})();
+`;
+
+const ThemeChanger = () => {
+  return (
+    <div>
+      <button onClick={() => setTheme("light")}>Light Mode</button>
+      <button onClick={() => setTheme("dark")}>Dark Mode</button>
+    </div>
+  );
+};
 export default App;
