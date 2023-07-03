11416aaa0bc26e9b4961c97727c6c6c66fc2777d
Rodrigo Pombo <pombopombopombo@gmail.com>
2023-07-03 09:56:50 +0000

    Add twitter card

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,18 @@
-import "@code-hike/mdx/dist/index.css";
-import "../global.css";
-import Head from "next/head";
+import "@code-hike/mdx/dist/index.css"
+import "../global.css"
+import Head from "next/head"
 
 function App({ Component, pageProps }) {
   return (
     <main>
       <Head>
         <title>OpenAI Chat Completion Unofficial Docs</title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="https://gpt.pomb.us/card.png" />
       </Head>
       <Component {...pageProps} />
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
